72bb2865b5d2cb7d125b1b329be47bc26ad7dc2d
Liam-Kim <liam.kim801@gmail.com>
2019-04-04 22:03:41 +0900

    minho_new_version

--- 21500830.c
+++ 21500830.c
@@ -1,6 +1,7 @@
 #include "21500830.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 
 int reverse_digits(int n)
@@ -16,3 +17,22 @@ int reverse_digits(int n)
 	return sum;
 }
 
+char* binarized(int n)
+{
+	char* bi = (char*)malloc(sizeof(char)*1);
+	char buff[2]={0,};
+	int i=0;
+	for(i=0; n!=0; i++){
+		bi = (char*)realloc(bi,sizeof(char)*(i+2));
+		sprintf(buff,"%d",n%2);
+		bi[i] = buff[0];
+		n = n/2;
+	}
+	//bi[i] = 0;
+	char *bi_re = (char*)calloc((i+1),sizeof(char));
+	for(int j=0; j!=i; j++){
+		bi_re[j] = bi[i-1-j];
+	}
+	free(bi);
+	return bi_re;
+}
